703c46dd8c896532a4ff672bf934284cf97b781c
lordbrad123 <33291644+lordbrad123@users.noreply.github.com>
2019-07-14 17:39:53 -0500

    Signed-off-by: lordbrad123 <33291644+lordbrad123@users.noreply.github.com>

--- backend/modelos/conexion.php
+++ backend/modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=bnluin93in23qh8qqu8o",
-						"umxuyzzpkgksvqil",
-						"Dq9tfTzMS4pLVoyq378Q",
+		$link = new PDO("mysql:host=http://bhjvavlb25hvzeh8zd6y-mysql.services.clever-cloud.com/;dbname=bhjvavlb25hvzeh8zd6y",
+						"uztri5oygjrd8twk",
+						"gQDg9jMBBBgp7aZJnJA7",
 						array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 		                      PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 						);
