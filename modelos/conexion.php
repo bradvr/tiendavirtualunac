bf143d83fe34a07fec1522ba5104867c4447f983
lordbrad123 <33291644+lordbrad123@users.noreply.github.com>
2019-07-14 17:24:23 -0500

    Signed-off-by: lordbrad123 <33291644+lordbrad123@users.noreply.github.com>

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=ecommerce",
-						"root",
-						"",
+		$link = new PDO("mysql:host=localhost;dbname=bnluin93in23qh8qqu8o",
+						"umxuyzzpkgksvqil",
+						"Dq9tfTzMS4pLVoyq378Q",
 						array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 		                      PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 						);
