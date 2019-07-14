d6de237434fbaf418980eeb02cac168e1837cbe3
lordbrad123 <33291644+lordbrad123@users.noreply.github.com>
2019-07-14 16:55:59 -0500

    Signed-off-by: lordbrad123 <33291644+lordbrad123@users.noreply.github.com>

--- modelos/rutas.php
+++ modelos/rutas.php
@@ -8,7 +8,7 @@ class Ruta{
 
 	public function ctrRuta(){
 
-		return "http://localhost/frontend/";
+		return "http://unacommerce.herokuapp.com/";
 	
 	}
 
@@ -18,7 +18,7 @@ class Ruta{
 
 	public function ctrRutaServidor(){
 
-		return "http://localhost/backend/";
+		return "http://unacommerce.herokuapp.com/backend/";
 	
 	}
 
