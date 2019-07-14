b57ded19c0f4d60311860844784b55c7681d4ddf
lordbrad123 <33291644+lordbrad123@users.noreply.github.com>
2019-07-14 16:34:45 -0500

    Signed-off-by: lordbrad123 <33291644+lordbrad123@users.noreply.github.com>

---
+++ backend/vistas/modulos/footer.php
@@ -0,0 +1,7 @@
+ <!-- main-footer -->
+<footer class="main-footer">
+
+<strong>Copyright &copy; 2019 <a href="#">Grupo Technology</a>.</strong> Todos los derechos reservados.
+
+</footer>
+<!-- main-footer -->
